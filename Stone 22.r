e65c6adced30faef0576f41f2f528c9dd3a5b4f9
Curtneyg <curtney.germaine@clarke.edu>
2024-05-02 13:53:41 -0500

    Add files via upload

---
+++ Stone 22.r
@@ -0,0 +1,30 @@
+#Stone 22
+
+title: "youth smoking"
+output: html_document
+Author: Curtney Germaine
+date: "2024-05-02"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## youth smoking data set
+author:Government of US
+url:https://catalog.data.gov/dataset/youth-tobacco-survey-yts-data
+date of access:Aug 23, 2023
+description:This data set shows us % of young people in different states that are smoking tabacco. This data set also gives us the exact number of the populations from different states that were sampled. You can also learn what year these surveys were taken. This Data set also indulged in cofidence level of these young students.
+
+
+YEAR:year
+LoacationAbbr:abbreviation of location
+LocationDesc:State 
+TopicType:Tabacco Use-Survey Data
+TopicDesc:Smoking tabacco use (Youth)
+MeasureDesc:User Status  /     percentage of current smokers who want to quit
+Education: High School, Middle School
+Response:N/A    / Current
+GeoLocation:Pinpoint locations of places around the world(where the survey's were taken)
+SampleSize:It is all different numbers of people in different states
+Gender:Male, female and Overall
